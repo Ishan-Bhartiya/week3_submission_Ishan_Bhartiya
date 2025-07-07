56349eb6f079205c130a9f39d43335763fc54372
Ishan-Bhartiya <ishanbhartiya@gmail.com>
2025-07-07 19:59:22 +0530

    Update Q1_Publisher.py

--- kratos_Ishan_Bhartiya/kratos_new_q1/kratos_new_q1/Q1_Publisher.py
+++ kratos_Ishan_Bhartiya/kratos_new_q1/kratos_new_q1/Q1_Publisher.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3      #Shebang for a python filere
+#importing required libraries
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
@@ -7,21 +8,21 @@ class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('Publisher')
-        self.publisher_=self.create_publisher(String, 'new',10)
+        self.publisher_=self.create_publisher(String, 'new',10) #Creating publisher object, which publishes String type variable on the topic new
         timer_period=1/15
-        self.timer=self.create_timer(timer_period,self.timer_callback)
+        self.timer=self.create_timer(timer_period,self.timer_callback) #Creating timer object,calls the timer_callback function every 1/15 seconds
 	
     def timer_callback(self):
-        msg=String()
+        msg=String() #Creating a string objct
         msg.data='Hello World !'
-        self.publisher_.publish(msg)
+        self.publisher_.publish(msg)#Publishes the msg over the topic
     	
 def main(args=None):
-    rclpy.init(args=args)
-    minimal_publisher=MinimalPublisher()
-    rclpy.spin(minimal_publisher)
+    rclpy.init(args=args) #initializes the ROS 2 Python client library, which is rclpy
+    minimal_publisher=MinimalPublisher()  #Creating an instance of the MinimalPublisher class
+    rclpy.spin(minimal_publisher) #Keeps running minimal publisher unless stopped
     
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':   #Runs the whole programme(sets __name__ to '__main__') if run directly, not if it's imported as a module into another script
     main()
