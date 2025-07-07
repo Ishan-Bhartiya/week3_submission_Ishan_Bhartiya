0e3c6adfb66b2e5a53a549f347eda7a80ec2b89d
Ishan-Bhartiya <ishanbhartiya@gmail.com>
2025-07-07 20:06:39 +0530

    Update Q1_Subscriber.py

--- kratos_Ishan_Bhartiya/kratos_new_q1/kratos_new_q1/Q1_Subscriber.py
+++ kratos_Ishan_Bhartiya/kratos_new_q1/kratos_new_q1/Q1_Subscriber.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3    #Shebang for any python file
+ #Importing necessary libraries and modules
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
@@ -6,18 +7,18 @@ from std_msgs.msg import String
 class MinimalSubscriber(Node):
 
     def __init__(self):
-        super().__init__('Subscriber')
-        self.subscriber=self.create_subscription(String,'new',self.listener_callback,10)
-        self.subscriber
+        super().__init__('Subscriber') #Sets this node's name to Subscriber
+        self.subscriber=self.create_subscription(String,'new',self.listener_callback,10)  #Creates subsciber object, receives String type message and then calls listener_callback
+        self.subscriber  #Prevents warnings on unused variables
     
-    def listener_callback(self,msg):
-        print(msg.data)
+    def listener_callback(self,msg): 
+        print(msg.data)                #Prints the data field of the message object
     	
 
 def main(args=None):
-    rclpy.init(args=args)
-    minimal_subscriber=MinimalSubscriber()
-    rclpy.spin(minimal_subscriber)
+    rclpy.init(args=args) #Initialises rclpy python library
+    minimal_subscriber=MinimalSubscriber() #creates an instance of the MinimalSubscriber class
+    rclpy.spin(minimal_subscriber) #Keeps on running the MinimalSubscriber till stopped
 
-if __name__ == '__main__':
+if __name__ == '__main__': #Sets __name__ to '__main__' if run directly, not if imported as a module into another script
     main()
